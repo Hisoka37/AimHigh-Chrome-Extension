eac7ae164fdbc805e8a748e8265f1a334930adf2
Salwa99 <s.ritage99@gmail.com>
2023-10-23 17:45:43 +0100

    Babel setup

---
+++ src/popup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const Popup = () => {
+  return (
+    <div>popup</div>
+  )
+}
+
+render(<Popup />, document.getElementById("react-target"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 module.exports = {
     mode: "development",
-    entry: "./src/test.jsx",
+    entry: "./src/popup.jsx",
     module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    }
+                }
+            }
+        ]
 
     },
+    resolve: {
+        extensions: ['.jsx', 'js']
+    },
+    
     output: {
-        filename: "index.js",
+        path: path.resole(__dirname, 'dist'),
+        filename: "[name].js",
     }
 }
