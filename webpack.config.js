8388e63aaba71b6a0ce8d08c46fcb4965f5216c5
Salwa99 <s.ritage99@gmail.com>
2023-10-23 18:15:06 +0100

    Plugin setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,44 @@
-module.exports = {
-    mode: "development",
-    entry: "./src/popup.jsx",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    }
-                }
-            }
-        ]
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-    },
-    resolve: {
-        extensions: ['.jsx', 'js']
-    },
-    
-    output: {
-        path: path.resole(__dirname, 'dist'),
-        filename: "[name].js",
-    }
-}
+module.exports = {
+  mode: "development",
+  entry: "./src/popup.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+},
+
+  plugins: [
+    new HtmlWebpackPlugin(
+        { 
+            template: "./src/popup.html",
+            filename: 'popup.html',
+         }),
+     new CopyPlugin({
+      patterns: [
+        { from: "public" },
+      ],
+    }),
+        ],
+
+  resolve: {
+    extensions: [".jsx", "js"],
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+};
