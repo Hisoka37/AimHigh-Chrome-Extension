39e0e4801caed6b1bca0cd057d52a2d7c64929e7
Walid Kabboussa <walidkabou@gmail.com>
2023-10-27 12:56:40 +0100

    fix code to add new components

---
+++ src/components/message.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className='bg-red'>This a message from a react component.</div>
+  )
+}
+
+export default Message
--- src/tabs/newtab.jsx
+++ src/tabs/newtab.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
 import '../assets/style.css'
+import Message from '../components/message.jsx'
 
 function Tab() {
   return (
-    <div className='bg-red-500'>
+    <div className='bg-red'>
       <h1 className='bg-green-500 text-3xl'>Hello Extension</h1>
       <p>This is a demo</p>
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
     </div>
   )
 }
