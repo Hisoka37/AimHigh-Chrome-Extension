31452c3a93299b66d12a922358133274d2989a7f
Salwa99 <s.ritage99@gmail.com>
2023-10-30 12:29:09 +0100

    Add axios

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import axios from 'axios'
 
 const Weather = () => {
   return (
-    <div>
+    <div className='bg-gray-100 shadow-xl w-[500px] h-[500px]'>
         <h1>Hello Weather</h1>
     </div>
   )
