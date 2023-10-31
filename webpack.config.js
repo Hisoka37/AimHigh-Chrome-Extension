8654a6ee088a0098be6f9fa8ce8ba79d0a644ce2
Salwa99 <s.ritage99@gmail.com>
2023-10-31 12:50:55 +0100

    Add more images

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    background: './src/background/background.js',
-    newtab: './src/tabs/newtab.jsx',
+    background: "./src/background/background.js",
+    newtab: "./src/tabs/newtab.jsx",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
   },
   module: {
     rules: [
@@ -17,24 +17,23 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
         test: /\.(jpg|jpeg|png|gif|svg)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              publicPath: '',
+              name: "images/[name].[ext]",
             },
           },
         ],
@@ -43,11 +42,14 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/tabs/newtab.html',
-      filename: 'newtab.html',
+      template: "./src/tabs/newtab.html",
+      filename: "newtab.html",
     }),
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [
+        { from: "public" },
+        { from: "src/assets/images", to: "images" }, // Add this line to copy images
+      ],
     }),
   ],
 };
