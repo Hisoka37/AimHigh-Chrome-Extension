bea1d1564d7df3da644be729cae365ef564bda25
Salwa99 <s.ritage99@gmail.com>
2023-10-31 15:29:07 +0100

    Add luxon

--- src/tabs/newtab.jsx
+++ src/tabs/newtab.jsx
@@ -3,12 +3,14 @@ import { render } from "react-dom";
 import "../assets/style.css";
 import Weather from "../components/weather/Weather.jsx";
 import RandomWallpaper from "../components/wallpaper/RandomWallpaper.jsx";
+import Geolocation from "../components/geolocation/Geolocation.jsx";
 
 function Tab() {
   return (
     <div className="relative">
       <RandomWallpaper />
       <Weather />
+      <Geolocation />
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: "public" },
-        { from: "src/assets/images", to: "images" }, // Add this line to copy images
+        { from: "src/assets/images", to: "images" },
       ],
     }),
   ],
