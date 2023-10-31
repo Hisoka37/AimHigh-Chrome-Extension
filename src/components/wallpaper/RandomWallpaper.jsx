829c5270d5d2d6b74479f1537738645c16670903
Salwa99 <s.ritage99@gmail.com>
2023-10-31 10:25:58 +0100

    Add file loader

---
+++ src/components/wallpaper/RandomWallpaper.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Img from "../../assets/images/img1.jpg"
+
+const RandomWallpaper = () => {
+  return (
+    <div>
+        <img src={Img} alt="wallpaper" />
+    </div>
+  )
+}
+
+export default RandomWallpaper
--- src/tabs/newtab.jsx
+++ src/tabs/newtab.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import "../assets/style.css";
 import Weather from "../components/weather/Weather.jsx";
+import RandomWallpaper from "../components/wallpaper/RandomWallpaper.jsx";
 
 function Tab() {
   return (
     <div className="relative">
+      <RandomWallpaper />
       <Weather />
     </div>
   );
